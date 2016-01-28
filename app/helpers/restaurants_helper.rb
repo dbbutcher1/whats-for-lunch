4bd2ee8c8d5569f8540ab52fdd61bb6b933970f3
David Butcher <dbbutcher1@gmail.com>
2016-01-28 15:38:00 -0500

    Adding seeds, debugging

--- app/helpers/restaurants_helper.rb
+++ app/helpers/restaurants_helper.rb
@@ -3,12 +3,20 @@ module RestaurantsHelper
   def random_restaurant(user, yesterdays_rating, address=Address.first)
     service = ApplicationHelper.places_service
 
-    restaurants = service.get_restaurants(address)
+    if address.nil?
+      # Cover case when we have no addresses in the database
+      restaurants = Restaurant.all
+    else
+      restaurants = service.get_restaurants(address)
+    end
+
+    # Make a selection!
     restaurant_choice = sort_and_weight_restaurants(user, restaurants).first
     
 
-    # Make sure we have a rating from yesterday
-    unless yesterdays_rating.nil?
+    # Make sure we have a rating from yesterday and more than 1 restuarant
+    # Don't want to get stuck in a loop.....
+    unless yesterdays_rating.nil? && !(restaurants.count > 1)
       # Make a new selection if the restaurants match
       while restaurant_choice.id == yesterdays_rating.restaurant.id do
         restaurant_choice = sort_and_weight_restaurants(user, restaurants)
@@ -19,12 +27,14 @@ module RestaurantsHelper
   end
 
   def ratings_by_date_and_user(user, date_to_query = DateTime.now.utc)
+    # Here to make finding ratings easier, since at one point we need to find yesterdays and todays rating.
     Rating.where(updated_at: date_to_query.beginning_of_day..date_to_query.end_of_day, user_id: user.id)
   end
 
   private
 
   def sort_and_weight_restaurants(user, restaurants)
+    # Give each restaurant a random number, then remove the weight of the rating and times eaten to give us a value
     weight_restaurants = []
 
     restaurants.each do |restaurant|
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,14 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+# Create our admin user
 User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password', role: 'admin')
+
+# Create a test user and give them an address
+user = User.create!(email: 'test@test.com', password: 'password', password_confirmation: 'password')
+Address.create!(user_id: user.id, zip_code: '30324')
+
+
+# Go ahead and seed a few restaurants
+RESTAURANTS = ['Subway', 'Chik-fil-a', 'McDonalds']
+
+RESTAURANTS.each do |restaurant|
+  Restaurant.create!(name: restaurant)
+end
